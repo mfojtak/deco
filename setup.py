f8c609bf0b22209d206d870f9eb5c067dcceadbc
Michal Fojtak <mfojtak@seznam.cz>
2019-07-23 11:40:14 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import codecs
+from setuptools import setup, find_packages
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
+
+setup(
+    name='mirbox',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/mfojtak/mirbox',
+    license='MIT',
+    author='mfojtak',
+    author_email='mfojtak@seznam.cz',
+    description='mirbox',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+    classifiers=(
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
