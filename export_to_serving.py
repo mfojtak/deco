6b275ae609cac56c7941e25018f74dcea92cc0bb
Michal Fojtak <mfojtak@seznam.cz>
2019-08-05 13:16:25 +0200

    Create export_to_serving.py

---
+++ export_to_serving.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+from tensorflow import keras
+from keras_bert import load_trained_model_from_checkpoint, bert
+import sys
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Exports keras model to serving format')
+parser.add_argument('--input', action="store", dest="input")
+parser.add_argument('--output', action="store", dest="output")
+
+args = parser.parse_args()
+
+model = keras.models.load_model(args.input, custom_objects = bert.get_custom_objects())
+tf.keras.experimental.export_saved_model(model, args.output, custom_objects = bert.get_custom_objects())
+
+#python export_to_serving.py --input /data/develop/logs/class_keras5/model_1.h5 --output /data/develop/models/class/1
