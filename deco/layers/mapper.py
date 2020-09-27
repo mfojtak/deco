114555212358165101e4428afd7c52bc7add0650
mfojtak <mfojtak@seznam.cz>
2020-09-27 14:44:31 +0000

    mapper fixed

--- deco/layers/mapper.py
+++ deco/layers/mapper.py
@@ -4,27 +4,30 @@ import sys
 
 class Mapper(tf.keras.layers.Layer):
 
-    def __init__(self, map, sort='DESCENDING'):
+    def __init__(self, map, sort='DESCENDING', batch_dims=0):
         super(Mapper, self).__init__()
         self.map = map
         self.map_tensor = tf.constant(map)
         self.sort = sort
+        self.batch_dims = batch_dims
 
     def call(self, input):
         indexes = self.map
         values = input
         if self.sort is not None:
             sorted_i = tf.argsort(input, direction=self.sort)
-            values = tf.gather(input, sorted_i)
+            print(sorted_i)
+            values = tf.gather(input, sorted_i, batch_dims=self.batch_dims)
+            print(values)
             indexes = tf.gather(self.map, sorted_i)
         return indexes, values
 
     def get_config(self):
-        return {'map': self.map, 'sort': self.sort}
+        return {'map': self.map, 'sort': self.sort, 'batch_dims': self.batch_dims}
 
 #import collections
-#t = tf.constant([1,2,3])
+#t = tf.constant([[1,2,3], [3,2,1], [1,4,2]])
 #print(isinstance((1,2), collections.Sequence))
-#mapper = Mapper([1234, 5678, 901234])
-#res = mapper([0.1, 0.9, 0.2])
+#mapper = Mapper([1234, 5678, 901234], batch_dims=1)
+#res = mapper(t)
 #print(res) 
